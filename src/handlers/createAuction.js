f936cc35f1148828a98871b167cf6647435f4e95
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-05-01 01:04:40 -0400

    feat: handler receives auction title and returns auction object

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,15 @@
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+
+  const auction = {
+    title,
+    status: 'open',
+    createdAt: new Date().toISOString(),
+  };
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello' }),
+    body: JSON.stringify({ auction }),
   };
 }
 
