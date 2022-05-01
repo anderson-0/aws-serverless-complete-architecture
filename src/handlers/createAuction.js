d8146f29ec835ba2f65a888984fe98ef17ba3e16
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-05-01 17:25:22 -0400

    chore: added @middy packages to facilitate http handling

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,5 +1,9 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -24,6 +28,9 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+   .use(httpJsonBodyParser())
+   .use(httpEventNormalizer())
+   .use(httpErrorHandler());
 
 
