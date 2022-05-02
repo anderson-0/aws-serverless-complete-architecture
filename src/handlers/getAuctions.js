94d8c7fdabb2b9d357abbd5bca23e8b09f251770
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-05-01 21:54:52 -0400

    feat: getAuctions query dynamodb using statusAndEndDateIndex GSI instead of using scan

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,17 +1,32 @@
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 
 import commonMiddleware from '../../lib/commonMiddleware';
+import getAuctionsSchema from '../../lib/schemas/getAuctionsSchema';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+
+  const { status } = event.queryStringParameters;
+
   let auctions;
 
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDateIndex',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status', // status is a reserved word in DynamoDB. We need to use #status to refer to it.
+    },
+  };
+
   try {
-    const result = await dynamoDb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise();
+    const result = await dynamoDb.query(params).promise();
 
     auctions = result.Items;
   } catch (error) {
@@ -25,4 +40,11 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions)
+  .use(validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true, // use default values for missing fields
+      strict: false,
+    }
+  }));
