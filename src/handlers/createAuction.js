501d8def2e26fdc65798895f6264c1f3c1691757
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-05-01 20:49:03 -0400

    fix: set endingAt to 1 hour after createdAt

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -12,7 +12,7 @@ async function createAuction(event, context) {
   const now = new Date();
   const endDate = new Date();
 
-  endDate.setHours(now.getDate() + 1);
+  endDate.setHours(now.getHours() + 1);
 
   const auction = {
     id: uuid(),
