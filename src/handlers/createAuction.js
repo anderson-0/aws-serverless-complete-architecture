b6fb3009271cfaeb453af0063f06abe79b2f60d7
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-05-01 22:07:53 -0400

    feat: apply schema validation to createAuction lambda

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,8 +1,10 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 
 import commonMiddleware from '../../lib/commonMiddleware';
+import createAuctionSchema from '../../lib/schemas/createAuctionSchema';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -41,4 +43,10 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({
+    inputSchema: createAuctionSchema,
+    ajvOptions: {
+      strict: false,
+    }
+  }));
